ec4aea68247605d8f1beff0d5e8b887c399000ab
Kanta Oikawa <iam@kantacky.com>
2025-06-20 01:17:34 +0900

    Update ci_scripts

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
+
 export REPOSITORY_DIR="/Volumes/workspace/repository"
 export APP_ROOT_DIR="$REPOSITORY_DIR/VibeCooking"
 echo $GOOGLE_SERVICE_INFO_PLIST > $APP_ROOT_DIR/GoogleService-Info.plist

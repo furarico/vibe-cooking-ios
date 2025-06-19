0dfd55f99bab683a10dc913f6f92d32954def87f
Kanta Oikawa <iam@kantacky.com>
2025-06-19 20:49:00 +0900

    Add Logger

---
+++ VibeCooking/Utility/Logger.swift
@@ -0,0 +1,88 @@
+//
+//  Logger.swift
+//  VibeCooking
+//
+//  Created by Kanta Oikawa on 2025/06/19.
+//
+
+import Foundation
+import os
+
+public enum Logger {
+    private static let standard = os.Logger(
+        subsystem: Bundle.main.bundleIdentifier!,
+        category: Category.standard.rawValue.capitalized
+    )
+
+    enum Category: String {
+        case standard
+    }
+
+    public enum Level: Int {
+        case debug
+        case info
+        case warning
+        case error
+
+        var label: String {
+            switch self {
+            case .debug:
+                return "􀀀 [DEBUG]"
+            case .info:
+                return "􀁞 [INFO]"
+            case .warning:
+                return "􀇾 [WARNING]"
+            case .error:
+                return "􀒉 [ERROR]"
+            }
+        }
+    }
+
+    public static func debug(_ items: Any..., file: String = #file, line: UInt = #line, function: String = #function) {
+        printItems(items, file: file, line: line, function: function, level: .debug)
+    }
+
+    public static func info(_ items: Any..., file: String = #file, line: UInt = #line, function: String = #function) {
+        printItems(items, file: file, line: line, function: function, level: .info)
+    }
+
+    public static func warn(_ items: Any..., file: String = #file, line: UInt = #line, function: String = #function) {
+        printItems(items, file: file, line: line, function: function, level: .warning)
+    }
+
+    public static func error(_ error: any Error, file: String = #file, line: UInt = #line, function: String = #function) {
+        printItems(error, file: file, line: line, function: function, level: .error)
+    }
+
+    public static func error(_ texts: String..., file: String = #file, line: UInt = #line, function: String = #function) {
+        printItems(texts, file: file, line: line, function: function, level: .error)
+    }
+
+    private static func printItems(_ items: Any..., file: String, line: UInt, function: String, level: Level) {
+        let thread = Thread.isMainThread ? "􀋽" : "􀋾"
+        let location = "\(URL(filePath: file).lastPathComponent):\(line) \(function)"
+        let date = DateFormatter.logger.string(from: Date())
+        let itemsText = items.map { "\($0)" }.joined(separator: " ")
+        let text = "\(level.label) \(thread) [\(date)] \(location) \(itemsText)"
+        switch level {
+        case .debug:
+            standard.debug("\(text)")
+        case .info:
+            standard.info("\(text)")
+        case .warning:
+            standard.warning("\(text)")
+        case .error:
+            standard.error("\(text)")
+        }
+    }
+}
+
+private extension DateFormatter {
+    static let logger = {
+        let formatter = DateFormatter()
+        formatter.locale = .current
+        formatter.timeZone = .current
+        formatter.dateFormat = "HH:mm:ss.SSS"
+        return formatter
+    }()
+}

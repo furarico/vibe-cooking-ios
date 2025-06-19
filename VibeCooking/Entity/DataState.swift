36197a8635165f004fd3ea7d8eef003b178a935d
Kanta Oikawa <iam@kantacky.com>
2025-06-19 20:51:47 +0900

    Add DataState

---
+++ VibeCooking/Entity/DataState.swift
@@ -0,0 +1,42 @@
+//
+//  DataState.swift
+//  VibeCooking
+//
+//  Created by Kanta Oikawa on 2025/06/19.
+//
+
+enum DataState<V, E>: Equatable, Sendable where V : Equatable & Sendable, E : Error & Equatable & Sendable {
+    case idle
+    case loading
+    case reloading(V)
+    case retrying(E)
+    case success(V)
+    case failure(E)
+
+    public var isLoading: Bool {
+        switch self {
+        case .loading, .reloading, .retrying:
+            true
+        default:
+            false
+        }
+    }
+
+    public var value: V? {
+        switch self {
+        case .reloading(let value), .success(let value):
+            value
+        default:
+            nil
+        }
+    }
+
+    public var error: E? {
+        switch self {
+        case .retrying(let error), .failure(let error):
+            error
+        default:
+            nil
+        }
+    }
+}

7d481865045f851f710038372aeb46634d25f1cc
Kanta Oikawa <iam@kantacky.com>
2025-06-19 21:53:07 +0900

    Add RecipeServiceError

---
+++ VibeCooking/Service/RecipeService/RecipeServiceError.swift
@@ -0,0 +1,15 @@
+//
+//  RecipeServiceError.swift
+//  VibeCooking
+//
+//  Created by Kanta Oikawa on 2025/06/19.
+//
+
+import Foundation
+
+enum RecipeServiceError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        }
+    }
+}

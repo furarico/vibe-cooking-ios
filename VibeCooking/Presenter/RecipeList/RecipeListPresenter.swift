6578cd7b82e03b7a7da4c9926d1d125ac06a6815
Kanta Oikawa <iam@kantacky.com>
2025-06-19 22:11:15 +0900

    Add RecipeListPresenter

---
+++ VibeCooking/Presenter/RecipeList/RecipeListPresenter.swift
@@ -0,0 +1,47 @@
+//
+//  RecipeListPresenter.swift
+//  VibeCooking
+//
+//  Created by Kanta Oikawa on 2025/06/19.
+//
+
+import Observation
+
+@Observable
+final class RecipeListPresenter<Environment: EnvironmentProtocol>: PresenterProtocol {
+    struct State: Equatable {
+        var recipes: DataState<[Components.Schemas.Recipe], DomainError> = .idle
+    }
+
+    enum Action {
+        case onAppear
+    }
+
+    var state = State()
+
+    private let recipeService = RecipeService<Environment>()
+
+    func dispatch(_ action: Action) {
+        Task {
+            await dispatch(action)
+        }
+    }
+
+    func dispatch(_ action: Action) async {
+        switch action {
+        case .onAppear:
+            await onAppear()
+        }
+    }
+}
+
+private extension RecipeListPresenter {
+    func onAppear() async {
+        do {
+            let recipes = try await recipeService.getRecipes()
+            state.recipes = .success(recipes)
+        } catch {
+            state.recipes = .failure(DomainError(error))
+        }
+    }
+}

b33566bd407ef5ec6b79cdecb1c13c07f979761f
Kanta Oikawa <iam@kantacky.com>
2025-06-20 17:48:34 +0900

    Update Environment

--- VibeCooking/Environment/Environment.swift
+++ VibeCooking/Environment/Environment.swift
@@ -11,6 +11,7 @@ protocol EnvironmentProtocol: Actor {
     static var shared: Self { get }
 
     var appCheckRepository: AppCheckRepositoryProtocol { get }
+    var audioRepository: AudioRepositoryProtocol { get }
     var recipeRepository: RecipeRepositoryProtocol { get }
     var speechRecognitionRepository: SpeechRecognitionRepositoryProtocol { get }
 }
@@ -19,15 +20,18 @@ final actor EnvironmentImpl: EnvironmentProtocol {
     static let shared: EnvironmentImpl = .init()
 
     let appCheckRepository: any AppCheckRepositoryProtocol
+    let audioRepository: any AudioRepositoryProtocol
     let recipeRepository: any RecipeRepositoryProtocol
     let speechRecognitionRepository: any SpeechRecognitionRepositoryProtocol
 
     init(
         appCheckRepository: any AppCheckRepositoryProtocol = AppCheckRepositoryImpl(),
+        audioRepository: any AudioRepositoryProtocol = AudioRepositoryImpl(),
         recipeRepository: any RecipeRepositoryProtocol = RecipeRepositoryImpl(),
         speechRecognitionRepository: any SpeechRecognitionRepositoryProtocol = SpeechRecognitionRepositoryImpl()
     ) {
         self.appCheckRepository = appCheckRepository
+        self.audioRepository = audioRepository
         self.recipeRepository = recipeRepository
         self.speechRecognitionRepository = speechRecognitionRepository
     }
--- VibeCooking/Mock/MockEnvironment.swift
+++ VibeCooking/Mock/MockEnvironment.swift
@@ -11,15 +11,18 @@ final actor MockEnvironment: EnvironmentProtocol {
     static let shared: MockEnvironment = .init()
 
     let appCheckRepository: any AppCheckRepositoryProtocol
+    let audioRepository: any AudioRepositoryProtocol
     let recipeRepository: any RecipeRepositoryProtocol
     let speechRecognitionRepository: any SpeechRecognitionRepositoryProtocol
 
     init(
         appCheckRepository: any AppCheckRepositoryProtocol = AppCheckRepositoryMock(),
+        audioRepository: any AudioRepositoryProtocol = AudioRepositoryMock(),
         recipeRepository: any RecipeRepositoryProtocol = RecipeRepositoryMock(),
         speechRecognitionRepository: any SpeechRecognitionRepositoryProtocol = SpeechRecognitionRepositoryMock()
     ) {
         self.appCheckRepository = appCheckRepository
+        self.audioRepository = audioRepository
         self.recipeRepository = recipeRepository
         self.speechRecognitionRepository = speechRecognitionRepository
     }

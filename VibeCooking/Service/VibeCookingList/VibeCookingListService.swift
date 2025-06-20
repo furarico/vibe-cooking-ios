3fa757d11aba394986018dd5369d665180db5b0b
Kanta Oikawa <iam@kantacky.com>
2025-06-21 01:16:40 +0900

    Add VibeCookingListService

--- VibeCooking/Service/VibeCookingList/VibeCookingListService.swift
+++ VibeCooking/Service/VibeCookingList/VibeCookingListService.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+final actor VibeCookingListService<Environment: EnvironmentProtocol> {
+    func getRecipes() async throws -> [Components.Schemas.Recipe] {
+        let recipeIDs: [String] = try await Environment.shared.localRepository.get(for: UserDefaultsKey.vibeCookingList)
+        return try await withThrowingTaskGroup(returning: [Components.Schemas.Recipe].self) { group in
+            recipeIDs.forEach { id in
+                group.addTask {
+                    try await Environment.shared.recipeRepository.fetchRecipe(id: id)
+                }
+            }
+            var recipes: [Components.Schemas.Recipe] = []
+            for try await recipe in group {
+                recipes.append(recipe)
+            }
+            return recipes
+        }
+    }
+
+    func addRecipe(id: String) async throws {
+        var recipeIDs: [String] = try await Environment.shared.localRepository.get(for: UserDefaultsKey.vibeCookingList)
+        guard !recipeIDs.contains(id) else { return }
+        recipeIDs.append(id)
+        try await Environment.shared.localRepository.set(recipeIDs, for: UserDefaultsKey.vibeCookingList)
+    }
+
+    func removeRecipe(id: String) async throws {
+        var recipeIDs: [String] = try await Environment.shared.localRepository.get(for: UserDefaultsKey.vibeCookingList)
+        guard let index = recipeIDs.firstIndex(of: id) else { return }
+        recipeIDs.remove(at: index)
+        try await Environment.shared.localRepository.set(recipeIDs, for: UserDefaultsKey.vibeCookingList)
+    }
+}

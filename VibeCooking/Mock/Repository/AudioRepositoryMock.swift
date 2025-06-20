defa91e2a67591ab6ddbcbf73f3001fd0702f95d
Kanta Oikawa <iam@kantacky.com>
2025-06-20 17:47:37 +0900

    Add AudioRepositoryMock

---
+++ VibeCooking/Mock/Repository/AudioRepositoryMock.swift
@@ -0,0 +1,16 @@
+//
+//  AudioRepositoryMock.swift
+//  VibeCooking
+//
+//  Created by Kanta Oikawa on 2025/06/20.
+//
+
+import Foundation
+
+final actor AudioRepositoryMock: AudioRepositoryProtocol {
+    func playAudio(from url: URL) async throws {
+    }
+    
+    func stopAudio() async {
+    }
+}

d6776d63729b1ecee734284086fbbcad6d6c889b
Kanta Oikawa <iam@kantacky.com>
2025-06-20 23:52:27 +0900

    Update CookingService

--- VibeCooking/Service/Cooking/CookingService.swift
+++ VibeCooking/Service/Cooking/CookingService.swift
@@ -8,12 +8,14 @@
 import Foundation
 
 final actor CookingService<Environment: EnvironmentProtocol> {
-    func startListening() -> AsyncStream<VoiceCommand> {
-        AsyncStream { continuation in
+    func startListening() async -> AsyncStream<VoiceCommand> {
+        await Environment.shared.audioRepository.stopAudio()
+        return AsyncStream { continuation in
             Task {
                 for await result in await Environment.shared.speechRecognitionRepository.startTranscribing() {
                     switch result {
                     case .success(let transcript):
+                        Logger.debug(transcript)
                         continuation.yield(VoiceCommand(transcript: transcript))
 
                     case .failure(let error):
@@ -25,12 +27,15 @@ final actor CookingService<Environment: EnvironmentProtocol> {
         }
     }
 
-    func stopListening() async {
+    func playAudio(url: URL, onFinished: @escaping () -> Void) async throws {
         await Environment.shared.speechRecognitionRepository.stopTranscribing()
+        Logger.debug("Playing audio from \(url)")
+        await Environment.shared.audioRepository.stopAudio()
+        try await Environment.shared.audioRepository.playAudio(from: url, onFinished: onFinished)
     }
 
-    func playAudio(url: URL) async throws {
+    func stopAll() async {
+        await Environment.shared.speechRecognitionRepository.stopTranscribing()
         await Environment.shared.audioRepository.stopAudio()
-        try await Environment.shared.audioRepository.playAudio(from: url)
     }
 }

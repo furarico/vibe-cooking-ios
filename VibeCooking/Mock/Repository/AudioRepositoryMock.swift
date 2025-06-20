55a72d7107c6a4805656f3c621d0576d4ac475b6
Kanta Oikawa <iam@kantacky.com>
2025-06-20 23:52:06 +0900

    Update AudioRepository

--- VibeCooking/Mock/Repository/AudioRepositoryMock.swift
+++ VibeCooking/Mock/Repository/AudioRepositoryMock.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 final actor AudioRepositoryMock: AudioRepositoryProtocol {
-    func playAudio(from url: URL) async throws {
+    func playAudio(from url: URL, onFinished: @escaping () -> Void) async throws {
     }
-    
+
     func stopAudio() async {
     }
 }
--- VibeCooking/Repository/Audio/AudioRepository.swift
+++ VibeCooking/Repository/Audio/AudioRepository.swift
@@ -9,21 +9,51 @@ import Foundation
 import AVFoundation
 
 protocol AudioRepositoryProtocol {
-    func playAudio(from url: URL) async throws
+    func playAudio(from url: URL, onFinished: @escaping () -> Void) async throws
     func stopAudio() async
 }
 
-final actor AudioRepositoryImpl: AudioRepositoryProtocol {
-    private var audioPlayer: AVAudioPlayer?
-    
-    func playAudio(from url: URL) async throws {
-        let data = try await URLSession.shared.data(from: url).0
-        audioPlayer = try AVAudioPlayer(data: data)
-        audioPlayer?.play()
+final actor AudioRepositoryImpl: NSObject, AudioRepositoryProtocol {
+    private var player: AVAudioPlayer?
+
+    private var onPlayingAudioFinished: (() -> Void)?
+
+    override init() {
+        super.init()
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playAndRecord, options: [.defaultToSpeaker, .duckOthers])
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            Logger.error("Failed to set audio session category: \(error)")
+        }
     }
-    
-    func stopAudio() async {
-        audioPlayer?.stop()
-        audioPlayer = nil
+
+    func playAudio(from url: URL, onFinished: @escaping () -> Void) async throws {
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(.playAndRecord, mode: .default, options: [.defaultToSpeaker, .duckOthers])
+        try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        onPlayingAudioFinished = onFinished
+        let data = try await URLSession.shared.data(from: url).0
+        player = try AVAudioPlayer(data: data)
+        player?.prepareToPlay()
+        player?.delegate = self
+        player?.play()
+    }
+
+    func stopAudio() {
+        player?.stop()
+        player = nil
+    }
+}
+
+extension AudioRepositoryImpl: AVAudioPlayerDelegate {
+    nonisolated func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        Logger.debug("Audio finished playing: \(flag)")
+        Task { [weak self] in
+            if let onPlayingAudioFinished = await self?.onPlayingAudioFinished {
+                onPlayingAudioFinished()
+            }
+        }
     }
 }

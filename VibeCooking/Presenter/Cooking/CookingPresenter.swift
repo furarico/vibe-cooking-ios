0ce7148b7320c532f515876fac3d7ce7eeeb1c47
Kanta Oikawa <iam@kantacky.com>
2025-06-20 23:52:44 +0900

    Update CookingPresenter

--- VibeCooking/Presenter/Cooking/CookingPresenter.swift
+++ VibeCooking/Presenter/Cooking/CookingPresenter.swift
@@ -5,8 +5,10 @@
 //  Created by Kanta Oikawa on 2025/06/19.
 //
 
+import AVFoundation
 import Foundation
 import Observation
+import SwiftUI
 
 @Observable
 final class CookingPresenter<Environment: EnvironmentProtocol>: PresenterProtocol {
@@ -31,7 +33,7 @@ final class CookingPresenter<Environment: EnvironmentProtocol>: PresenterProtoco
     enum Action {
         case onAppear
         case onDisappear
-        case onInstructionAppear(Components.Schemas.Instruction)
+        case onInstructionChanged(Components.Schemas.Instruction)
     }
 
     var state: State
@@ -56,44 +58,61 @@ final class CookingPresenter<Environment: EnvironmentProtocol>: PresenterProtoco
         case .onDisappear:
             await onDisappear()
 
-        case .onInstructionAppear(let instruction):
-            await onInstructionAppear(instruction: instruction)
+        case .onInstructionChanged(let instruction):
+            await onInstructionChanged(instruction: instruction)
         }
     }
 }
 
 private extension CookingPresenter {
     func onAppear() async {
-        Task {
-            for await voiceCommand in await cookingService.startListening() {
-                switch voiceCommand {
-                case .goBack:
-                    if state.currentInstructionStep > 1 {
-                        state.currentInstructionStep -= 1
-                    }
-                case .goForward:
-                    if state.currentInstructionStep < state.recipe.instructions.count {
-                        state.currentInstructionStep += 1
-                    }
-                case ._repeat:
-                    break
-                case .none:
-                    break
+        UIApplication.shared.isIdleTimerDisabled = true
+        guard let instruction = state.recipe.instructions.sorted(by: { $0.step < $1.step }).first else {
+            return
+        }
+        await playAudio(of: instruction)
+    }
+
+    func onDisappear() async {
+        UIApplication.shared.isIdleTimerDisabled = false
+        await cookingService.stopAll()
+    }
+
+    func onInstructionChanged(instruction: Components.Schemas.Instruction) async {
+        await playAudio(of: instruction)
+    }
+}
+
+private extension CookingPresenter {
+    func startSpeechRecognition() async {
+        for await voiceCommand in await cookingService.startListening() {
+            switch voiceCommand {
+            case .goBack:
+                if state.currentInstructionStep > 1 {
+                    state.currentInstructionStep -= 1
                 }
+            case .goForward:
+                if state.currentInstructionStep < state.recipe.instructions.count {
+                    state.currentInstructionStep += 1
+                }
+            case ._repeat:
+                break
+            case .none:
+                break
             }
         }
     }
 
-    func onDisappear() async {
-        await cookingService.stopListening()
-    }
-
-    func onInstructionAppear(instruction: Components.Schemas.Instruction) async {
+    func playAudio(of instruction: Components.Schemas.Instruction) async {
         guard let url = URL(string: instruction.audioUrl ?? "") else {
             return
         }
         do {
-            try await cookingService.playAudio(url: url)
+            try await cookingService.playAudio(url: url) { [weak self] in
+                Task {
+                    await self?.startSpeechRecognition()
+                }
+            }
         } catch {
             Logger.error(error)
         }

1794a432c4c30479fdd60e693c3836cd602f8511
Kanta Oikawa <iam@kantacky.com>
2025-10-31 18:13:03 +0900

    Add watch app

---
+++ VibeCookingWatchApp/VibeCookingWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeCookingWatchApp.swift
+//  VibeCookingWatchApp
+//
+//  Created by Kanta Oikawa on 2025/10/31.
+//
+
+import SwiftUI
+
+@main
+struct VibeCookingWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VibeCookingWatchAppTests/VibeCookingWatchAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  VibeCookingWatchAppTests.swift
+//  VibeCookingWatchAppTests
+//
+//  Created by Kanta Oikawa on 2025/10/31.
+//
+
+import Testing
+@testable import VibeCookingWatchApp
+
+struct VibeCookingWatchAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
